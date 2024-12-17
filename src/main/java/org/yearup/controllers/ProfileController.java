c38ac9cbbeca412ae531c95815f036d4ca8225a8
Fran-Santos <francihelen.santos25@gmail.com>
2024-12-17 00:30:35 -0800

    added get profile by user id method

--- src/main/java/org/yearup/controllers/ProfileController.java
+++ src/main/java/org/yearup/controllers/ProfileController.java
@@ -3,6 +3,7 @@ package org.yearup.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,9 +15,10 @@ import org.yearup.models.User;
 
 import java.security.Principal;
 
-//Start based on ProductsController
+//Start based on ProductsController class
 @RestController
 @RequestMapping("profile")
+@CrossOrigin
 public class ProfileController {
     private ProfileDao profileDao;
     private UserDao userDao;
@@ -26,21 +28,21 @@ public class ProfileController {
         this.profileDao = profileDao;
         this.userDao = userDao;
     }
-//Get profile method to get user profile, Update method to update user profile
-
+//Get profile method to get user profile,
     @GetMapping("")
-    @PreAuthorize("isAuthenticated()")
+    @PreAuthorize("isAuthenticated()") // is authenticated because just logging in user should have a profile
     public Profile getProfile(Principal principal){
         try
-        {
+        { //Example from ShoppingCartController class method getCart
+
             // get the currently logged in username
             String userName = principal.getName();
             // find database user by userId
             User user = userDao.getByUserName(userName);
             int userId = user.getId();
 
-            // use the shoppingcartDao to get all items in the cart and return the cart
-            return null;
+            // use the profileDao to get the profile of the user and return it
+            return profileDao.getByUserId(userId); //calling getUserById because that is what the method needs to return
         }
         catch(Exception e)
         {
