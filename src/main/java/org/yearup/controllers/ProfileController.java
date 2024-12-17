9f31da1e75eb2f2dd6662e1c032293965b0490d8
Fran-Santos <francihelen.santos25@gmail.com>
2024-12-16 23:21:19 -0800

    Updated ProfileDao interface

--- src/main/java/org/yearup/controllers/ProfileController.java
+++ src/main/java/org/yearup/controllers/ProfileController.java
@@ -1,21 +1,52 @@
 package org.yearup.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 import org.yearup.data.ProfileDao;
+import org.yearup.data.UserDao;
+import org.yearup.models.Profile;
+import org.yearup.models.User;
+
+import java.security.Principal;
 
 //Start based on ProductsController
 @RestController
 @RequestMapping("profile")
 public class ProfileController {
     private ProfileDao profileDao;
+    private UserDao userDao;
 
     @Autowired
-    public ProfileController(ProfileDao profileDao) {
+    public ProfileController(ProfileDao profileDao, UserDao userDao) {
         this.profileDao = profileDao;
+        this.userDao = userDao;
     }
 //Get profile method to get user profile, Update method to update user profile
+
+    @GetMapping("")
+    @PreAuthorize("isAuthenticated()")
+    public Profile getProfile(Principal principal){
+        try
+        {
+            // get the currently logged in username
+            String userName = principal.getName();
+            // find database user by userId
+            User user = userDao.getByUserName(userName);
+            int userId = user.getId();
+
+            // use the shoppingcartDao to get all items in the cart and return the cart
+            return null;
+        }
+        catch(Exception e)
+        {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Oops... our bad.");
+        }
+    }
 
 }
 
--- src/main/java/org/yearup/data/ProfileDao.java
+++ src/main/java/org/yearup/data/ProfileDao.java
@@ -1,9 +1,12 @@
 package org.yearup.data;
-
-
 import org.yearup.models.Profile;
 
+
+// You will need to update the DAO to add the getByUserID and update methods
 public interface ProfileDao
 {
-    Profile create(Profile profile);
+    Profile create(Profile profile); //Create a profile in the database when the user register
+    Profile getByUserId(int userId); // it returns profile object when a userId is given
+    void update(int userId, Profile profile); //update a profile, based on interfaceProductDao
+
 }
